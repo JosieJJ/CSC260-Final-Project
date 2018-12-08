aa28b6ca460fa74283042b410547639a85eed94e
JosieJJ <45248200+JosieJJ@users.noreply.github.com>
2018-12-07 19:09:21 -0600

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace Notebook__Final_
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
